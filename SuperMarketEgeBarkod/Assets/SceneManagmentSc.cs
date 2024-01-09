70bc245b164c6000234cd322761b0dfa820bd028
EmirGaziKopar <1190505016@ogrenci.kirklareli.edu.tr>
2024-01-09 22:15:37 +0300

    1.6

---
+++ SuperMarketEgeBarkod/Assets/SceneManagmentSc.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagmentSc : MonoBehaviour
+{
+    
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
+    }
+    public void CreditsButton()
+    {
+
+    }
+    public void ExitGame()
+    {
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
